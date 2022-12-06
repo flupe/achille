7b196d2045a6ac8aecee121da7c4d779a931a744
flupe <lucas@escot.me>
2022-12-06 22:40:29 +0100

    put back IO interface

--- achille/Achille/IO.hs
+++ achille/Achille/IO.hs
@@ -1,40 +1,19 @@
--- | Defines an IO interface for core recipes
-module Achille.Internal.IO
-    ( AchilleIO
-    , readFile
-    , readFileLazy
-    , copyFile
-    , writeFile
-    , writeFileLazy
-    , doesFileExist
-    , doesDirExist
-    , callCommand
-    , readCommand
-    , log
-    , glob
-    , getModificationTime
-    , fail
-    ) where
+-- | Exposes an IO interface used by core achille recipes
+module Achille.IO (AchilleIO(..)) where
 
-import Prelude as Prelude hiding (log, readFile, writeFile)
+import Data.Text        (Text)
+import Data.Time.Clock  (UTCTime)
 
-import Data.Text          (Text)
-import System.FilePath    (FilePath)
-import Data.Time.Clock    (UTCTime)
-import Control.Monad.Fail (MonadFail)
-
-import qualified System.Directory     as Directory
-import qualified System.FilePath      as FilePath
-import qualified System.FilePath.Glob as Glob
-import qualified System.Process       as Process
-import qualified Data.Text            as Text
-import qualified Data.Text.IO         as TextIO
-import qualified Data.ByteString      as BS
-import qualified Data.ByteString.Lazy as LBS
+import Data.ByteString      qualified as BS
+import Data.ByteString.Lazy qualified as LBS
+import System.Directory     qualified as Directory
+import System.FilePath      qualified as FilePath
+import System.FilePath.Glob qualified as Glob
+import System.Process       qualified as Process
 
 
--- | Interface for IO actions used by core recipes.
-class (Monad m, MonadFail m) => AchilleIO m where
+-- | Interface for IO operations used by core recipes.
+class AchilleIO m where
     -- | Retrieve a file as a bytestring.
     readFile            :: FilePath -> m BS.ByteString
     -- | Retrieve a file as a /lazy/ bytestring.
@@ -50,11 +29,11 @@ class (Monad m, MonadFail m) => AchilleIO m where
     -- | Check whether a directory exists.
     doesDirExist        :: FilePath -> m Bool
     -- | Run a shell command in a new process.
-    callCommand         :: String   -> m ()
+    callCommand         :: String -> m ()
     -- | Run a shell command in a new process.
-    readCommand         :: String   -> [String] -> m String
+    readCommand         :: String -> [String] -> m String
     -- | Log a string to stdout.
-    log                 :: String   -> m ()
+    log                 :: String -> m ()
     -- | Find all paths matching a given globpattern, relative to a given directory.
     glob                :: FilePath -- ^ Path of the root directory.
                         -> Glob.Pattern   -> m [FilePath]
@@ -70,16 +49,16 @@ ensureDirExists =
 instance AchilleIO IO where
     readFile             = BS.readFile
     readFileLazy         = LBS.readFile
-    copyFile from to     = ensureDirExists to >> Directory.copyFile from to
-    writeFile to x       = ensureDirExists to >> BS.writeFile to x
-    writeFileLazy to x   = ensureDirExists to >> LBS.writeFile to x
+    copyFile from to     = ensureDirExists to *> Directory.copyFile from to
+    writeFile to x       = ensureDirExists to *> BS.writeFile to x
+    writeFileLazy to x   = ensureDirExists to *> LBS.writeFile to x
     doesFileExist        = Directory.doesFileExist
     doesDirExist         = Directory.doesDirectoryExist
     callCommand          = Process.callCommand
     readCommand cmd args = Process.readProcess cmd args []
     log                  = Prelude.putStrLn
     glob dir pattern     =
-        Directory.withCurrentDirectory dir $
-            Glob.globDir1 pattern ""
-            >>= mapM Directory.makeRelativeToCurrentDirectory
+      Directory.withCurrentDirectory dir $
+          Glob.globDir1 pattern ""
+          >>= mapM Directory.makeRelativeToCurrentDirectory
     getModificationTime  = Directory.getModificationTime
