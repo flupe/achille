b93c74e86fb27383389386813dc73366e3d9a05d
flupe <lucas@escot.me>
2022-12-28 14:41:18 +0100

    working example with stache templates

--- achille-stache/Achille/Stache/Recipe.hs
+++ achille-stache/Achille/Stache/Recipe.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneDeriving, RecordWildCards #-}
 module Achille.Stache.Recipe where
 
 import Prelude hiding ((.), id)
@@ -9,10 +9,11 @@ import Data.Binary (Binary)
 import Data.Aeson (ToJSON(toJSON))
 import Data.Text.Lazy (Text)
 import Data.Map.Strict (Map)
-import System.FilePath (takeExtension)
+import System.FilePath ((</>))
 import Text.Mustache
 import Text.Megaparsec.Pos (Pos)
 
+import Achille.Cache
 import Achille.Diffable
 import Achille.IO
 import Achille.Recipe
@@ -25,9 +26,25 @@ deriving instance Binary Pos
 deriving instance Binary Node
 deriving instance Binary Template
 
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM b x = b >>= \case
+  True  -> pure ()
+  False -> x
+
 
 loadTemplate :: Recipe IO FilePath Template
-loadTemplate = undefined
+loadTemplate = embed Embedded
+  { rName = "loadTemplate"
+  , runEmbed = \Context{..} cache v@(src, _) -> do
+      let path = inputRoot </> src -- TODO(flupe): whether to add currentDir or not is problematic
+      unlessM (doesFileExist path) $ fail $ "Could not find template file " <> path
+      mtime <- getModificationTime path
+      case fromCache cache of
+        Just (t :: Template) | mtime <= lastTime, not (hasChanged v) -> pure (value t False, cache)
+        _ -> do
+          t <- compileMustacheFile path -- TODO(flupe): handle parser exception gracefully
+          pure (value t True, toCache t)
+  }
 
 
 loadTemplates :: Recipe IO FilePath (Map PName Template)
--- example/Main.hs
+++ example/Main.hs
@@ -3,14 +3,12 @@ module Main where
 import Prelude
 import Control.Monad (forM_)
 import Data.Text     (Text, pack)
-import Data.Aeson    (FromJSON)
-import Data.Binary   (Binary)
+import Data.Aeson    (FromJSON, ToJSON)
 import GHC.Generics  (Generic)
-import Lucid
-import Lucid.Base qualified as Lucid
 
 import Achille as A
 import Achille.Pandoc
+import Achille.Stache
 import Achille.Writable qualified as Writable
 
 -- things to add to Achille.Syntax
@@ -19,44 +17,21 @@ sortOn :: (Achille task, Ord b) => (a -> b) -> task m [a] -> task m [a]
 sortOn = undefined
 
 -- post meta information
-data Meta = Meta
-  { title :: Text
-  , date  :: Text
-  } deriving (Generic, FromJSON, Binary)
+-- data Meta = Meta
+--   { title :: Text
+--   , date  :: Text
+--   } deriving (Generic, FromJSON, Binary)
+
+data Post = Post
+  { content :: Text
+  } deriving (Generic, ToJSON)
 
 
 -- main example
 main :: IO ()
 main = achille A.do
-  -- posts <-
-    match_ "posts/*.md" \src -> A.do
-      txt <- processPandoc src
-      write (src -<.> "html") (renderPost <$> txt)
+  template <- loadTemplate "templates/post.html"
 
-  -- write undefined (renderIndex <$> sortOn (date . Prelude.fst) posts)
-
--- telling achille how to write Lucid HTML to file
-instance Writable.Writable IO (Html ()) where
-  write p = Writable.write p . Lucid.renderBS
-
-
-outer :: Html () -> Html ()
-outer body = doctypehtml_ do
-  header_ $ h1_ "My beautiful site"
-  main_ body
-  footer_ $ p_ "2022 All rights reserved"
-
-
-renderPost :: Text -> Html ()
-renderPost content = outer $ do
-  article_ do
-    -- h1_      $ toHtml title
-    -- p_       $ "Published on " <> toHtml date
-    section_ $ toHtmlRaw content
-
-
-renderIndex :: [(Meta, FilePath)] -> Html ()
-renderIndex posts = outer do
-  h2_ "Articles"
-  ul_ $ forM_ posts \(Meta{..}, url) -> li_ do
-    a_ [href_ (pack url)] $ toHtml title
+  match_ "posts/*.md" \src -> A.do
+    post <- Post <$> processPandoc src
+    write (src -<.> "html") (applyTemplate template post)
